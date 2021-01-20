a0ab6283fe78a33718f7e317e247e1aa20426144
alumno <plajommj@hotmail.com>
2021-01-20 08:13:32 +0100

    Tercer día con la api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        self::seedUsers();
+    }
+
+    public static function seedUsers(){
+        User::truncate();
+        $user = new User();
+        $user->name = 'Pablo';
+        $user->email = 'plajommj@hotmail.com';
+        $user->password = '123456';
+        $user->save();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
 use Tqdev\PhpCrudApi\Api;
 use Tqdev\PhpCrudApi\Config;
+use App\Http\Controllers\API\CentroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ use Tqdev\PhpCrudApi\Config;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('centros',CentroController::class);
 
 Route::any('/{any}', function (ServerRequestInterface $request) {
     $config = new Config([
